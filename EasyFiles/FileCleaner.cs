ab3c8a2bf8634e8674d3a56db135f5fc9fce884e
土豆泥拌辣椒 <1164392802@qq.com>
2022-11-09 10:40:15 +0800

    init project

---
+++ EasyFiles/FileCleaner.cs
@@ -0,0 +1,30 @@
+﻿namespace EasyFiles
+{
+    public class FileCleaner
+    {
+        private string path;
+        public FileCleaner(string path)
+        {
+            this.path = path;
+        }
+
+        public bool deleteByDay(int day)
+        {
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo dic = new DirectoryInfo(path);
+                var allFile = dic.GetFiles();
+                foreach (var file in allFile)
+                {
+                    if (file.LastWriteTime < DateTime.Now.AddDays(-day))
+                    {
+                        file.Delete();
+                    }
+                }
+                return true;
+            }
+            else  return false; 
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TP.Heart
 asp dot 6 tools  for common
+
+Each subproject has a separate description
